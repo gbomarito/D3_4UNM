Fixed units and things

DislocationMngr.py
@@ -29,19 +29,22 @@ class DislocationMngr(object):
         
         self.dislocations=[]
         
-        '''
-        self.dislocations.append(Dislocation.Dislocation( np.array((-sim_size/2.,0.,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)) ))
-        self.dislocations.append(Dislocation.Dislocation( np.array((sim_size/2.,sim_size/4.,0.)), np.array((-b,0.,0.)), np.array((0.,1.,0.)) ))
+        
+        self.dislocations.append(Dislocation.Dislocation( np.array((-sim_size/2.,sim_size/2.,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)) ))
+        self.dislocations.append(Dislocation.Dislocation( np.array((sim_size/2.,3.*sim_size/4.,0.)), np.array((-b,0.,0.)), np.array((0.,1.,0.)) ))
         self.dislocations.append(Dislocation.Dislocation( np.array((sim_size*3./4.,0.,0.)), np.array((-b,0.,0.)), np.array((0.,1.,0.)) ))
         self.dislocations.append(Dislocation.Dislocation( np.array((sim_size*-3./4.,0.,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)) ))
         self.dislocations.append(Dislocation.Dislocation( np.array((sim_size*-5./4.,sim_size/2.,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)) ))
         self.dislocations.append(Dislocation.Dislocation( np.array((0.,-sim_size/2.,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)) ))
-        self.dislocations.append(Dislocation.Dislocation( np.array((0.,sim_size/2.,0.)), np.array((-b,0.,0.)), np.array((0.,1.,0.)) ))'''
+        self.dislocations.append(Dislocation.Dislocation( np.array((0.,sim_size/2.,0.)), np.array((-b,0.,0.)), np.array((0.,1.,0.)) ))
 
         self.sources=[]
         L_nuc = 40.*b
-        self.sources.append(DislocationSrc.DislocationSrc( np.array((-sim_size/2.0,-sim_size/2.,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)), L_nuc, self.mu, self.nu ))
-        self.sources.append(DislocationSrc.DislocationSrc( np.array((sim_size/2.0,sim_size/2.,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)), L_nuc, self.mu, self.nu ))
+        drag=1.e-3 #poise
+        t_nuc = 2.6e6*drag/m
+        print "t_nuc: ", t_nuc
+        self.sources.append(DislocationSrc.DislocationSrc( np.array((-.5*sim_size,-1.25*sim_size,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)), L_nuc, t_nuc, self.mu, self.nu ))
+        self.sources.append(DislocationSrc.DislocationSrc( np.array((sim_size/2.0,sim_size/2.,0.)), np.array((b,0.,0.)), np.array((0.,1.,0.)), L_nuc, t_nuc, self.mu, self.nu ))
         
         
         
@@ -88,12 +91,8 @@ class DislocationMngr(object):
                 
             #far field stress
             if sig_ff is not None:
-<<<<<<< HEAD
                 sigma_ext[i] += sig_ff(self.dislocations[i].X)
                 sigma[i] += sigma_ext[i]
-=======
-                sigma[i] += sig_ff(d_i.X)
->>>>>>> 91ad96a02515f75068a6da30cad8788d5ea40aed
                 
             #TODO add FE stress field  
             
@@ -190,17 +189,18 @@ class DislocationMngr(object):
             #self.velocities_last=[None]*len(self.dislocations) #reset velocities so energy balance works in next step
 
         #Nucleate dislocations at sources
-        """for i in self.sources:
+        for i in self.sources:
             thissig = self.stress_at_point(i.X)
             #far field stress
             if sig_ff is not None:
                 thissig += sig_ff(i.X)
-            res = i.load(thissig,self.mu,self.nu, self.L_glide)
+            res = i.load(thissig,self.mu,self.nu, self.L_glide, dt)
             if len(res) > 0:
                 self.E_bal_skip=True
                 for j in res:
                     self.dislocations.append(j)
-                    self.velocities_last.append(None)#0.0?"""
+                    self.velocities_last.append(None)#0.0?
+                    self.velocities_last_ext.append(None)
             
         
         # add reaction products
@@ -272,7 +272,7 @@ class DislocationMngr(object):
     def plot_w_stress(self,filename,sig_ff=None,FE_results=None):
         """plot of all dislocations with a stress contour"""
 
-        bound = 1e7
+        bound = 1.e9
         
         #find stress field
         delta = 0.05*self.sim_size
@@ -310,6 +310,8 @@ class DislocationMngr(object):
         for d in self.dislocations:
             theta=180.0/np.pi * math.atan2(d.burgers[1],d.burgers[0])
             plt.text(d.X[0],d.X[1],r'$\perp$',ha='center',rotation=theta,va='center')
+        for s in self.sources:
+            plt.text(s.X[0],s.X[1],'o')
         plt.colorbar()
         pylab.savefig(filename+"_xx.png")
         plt.close()
@@ -321,6 +323,8 @@ class DislocationMngr(object):
         for d in self.dislocations:
             theta=180.0/np.pi * math.atan2(d.burgers[1],d.burgers[0])
             plt.text(d.X[0],d.X[1],r'$\perp$',ha='center',rotation=theta,va='center')
+        for s in self.sources:
+            plt.text(s.X[0],s.X[1],'o')
         plt.colorbar()
         pylab.savefig(filename+"_yy.png")
         plt.close()
@@ -332,6 +336,8 @@ class DislocationMngr(object):
         for d in self.dislocations:
             theta=180.0/np.pi * math.atan2(d.burgers[1],d.burgers[0])
             plt.text(d.X[0],d.X[1],r'$\perp$',ha='center',rotation=theta,va='center')
+        for s in self.sources:
+            plt.text(s.X[0],s.X[1],'o')
         plt.colorbar()
         pylab.savefig(filename+"_zz.png")
         plt.close()
@@ -343,6 +349,8 @@ class DislocationMngr(object):
         for d in self.dislocations:
             theta=180.0/np.pi * math.atan2(d.burgers[1],d.burgers[0])
             plt.text(d.X[0],d.X[1],r'$\perp$',ha='center',rotation=theta,va='center')
+        for s in self.sources:
+            plt.text(s.X[0],s.X[1],'o')
         plt.colorbar()
         pylab.savefig(filename+"_xy.png")
         plt.close()

DislocationSrc.py
@@ -1,8 +1,9 @@
 import numpy as np
+import Dislocation
 
 class DislocationSrc(object):
 
-    def __init__(self, location, bv, sp, L_nuc, mu, nu):
+    def __init__(self, location, bv, sp, L_nuc, t_nuc, mu, nu):
         """Returns an initialized dislocation source object.  Only sources 
            with line vectors in the z direction are possible.  L_nuc
             should be much larger than L_annihilation, perhaps by a factor of 2.
@@ -11,14 +12,21 @@ class DislocationSrc(object):
         self.burgers=np.copy(bv)
         self.line_vec=np.array((0,0,1))
         self.slip_plane=np.copy(sp)/np.linalg.norm(sp)
+        self.L_nuc = L_nuc
         self.tau_nuc = mu*np.linalg.norm(bv)/(2*np.pi*(1-nu)*L_nuc)
+        self.t_nuc = t_nuc
+        self.load_time = 0.
         
 
-    def load(self,sigma,mu,nu, L_glide):
+    def load(self,sigma,mu,nu, L_glide, dt):
         products = []
-        tau = np.dot(self.burgers,np.dot(sigma,self.slip_plane))
+        tau = np.dot(self.burgers/np.linalg.norm(self.burgers),np.dot(sigma,self.slip_plane))
         if abs(tau)>self.tau_nuc:
-            L = mu*np.linalg.norm(bv)/(2*np.pi*(1-nu)*tau)
+            self.load_time += dt*np.sign(tau)
+        else:
+            self.load_time = 0
+        if abs(self.load_time)>=self.t_nuc:
+            L = mu*np.linalg.norm(self.burgers)/(2*np.pi*(1-nu)*tau)
             if abs(L)<L_glide:
                 print "These dislocations will immediately annihilate!!!"
                 #We could add two dipoles?  Output this error and reset the time step?
@@ -30,9 +38,10 @@ class DislocationSrc(object):
                 products.append(Dislocation.Dislocation( self.X - Xstep, -self.burgers, self.slip_plane ))
                 tau = tau - mu*np.linalg.norm(bv)/(2*np.pi*(1-nu)*1.1*L_glide))
                 L = mu*np.linalg.norm(bv)/(2*np.pi*(1-nu)*tau))"""
-            Xstep = L*self.burgers/np.linalg.norm(self.burgers)/2.
+            Xstep = np.sign(self.load_time)*self.L_nuc*self.burgers/np.linalg.norm(self.burgers)/2.
             products.append(Dislocation.Dislocation( self.X + Xstep, self.burgers, self.slip_plane ))
-            products.append(Dislocation.Dislocation( self.X - Xstep, -self.burgers, self.slip_plane ))            
+            products.append(Dislocation.Dislocation( self.X - Xstep, -self.burgers, self.slip_plane ))
+            self.load_time = 0           
         return products
 
     def rotate(self,beta):

Driver.py
@@ -2,18 +2,15 @@ import numpy as np
 import os
 from DislocationMngr import DislocationMngr
 
-<<<<<<< HEAD
 ################################################################################
 def const_shear(loc):
-    shear=4.75e5
+    shear=2e6
     return np.array(((0,shear,0),(shear,0,0),(0,0,0)))
-=======
-def simple_shear():
-        sig = sigma=np.zeros((3,3))
-        sig[0,1] = 2700000.
-        sig[1,0] = sig[0,1]
-        return sig
->>>>>>> 91ad96a02515f75068a6da30cad8788d5ea40aed
+
+def variable_shear(loc, t):
+    shear = 1.8e6#1.3e6 + .5e6*t
+    return np.array(((0,shear,0),(shear,0,0),(0,0,0)))
+
 
 ################################################################################
 if __name__ == "__main__":
@@ -26,26 +23,26 @@ if __name__ == "__main__":
     
     #----:: INPUT ::----
     # simulation params
-    starting_dt=1e-2
-    sim_time=1e-0
+    starting_dt=1e-9
+    sim_time=2.e-8
     
     # output params
-    plot_res=sim_time/50.
+    plot_res=sim_time/100.
     
     # material params
     nu=0.3
-    mu=270000000.
-    drag=1.0e4
-    burgers=4.05e-8/(2.**.5)
+    mu=27.e10 #dyne/cm^2
+    drag=1.e-3 #poise
+    burgers=4.05e-8/(2.**.5) #cm
     
     # integration/discretization params
-    energy_tol=1e-3
+    energy_tol=1.e-3
     
     
     
     #----:: ININTIALIZATION ::----
     # initialize dislocation manager
-    dm=DislocationMngr(nu,mu,drag,dnum=20, b=burgers, sim_size=burgers*100, ad_climb=5*burgers, ad_glide=20*burgers)
+    dm=DislocationMngr(nu,mu,drag,dnum=20, b=burgers, sim_size=burgers*100, ad_climb=1*burgers, ad_glide=5*burgers)
     #dm.plot_w_stress("sig{0:04d}".format(0))
     dm.plot("D{0:04d}".format(0))  
     
@@ -60,7 +57,7 @@ if __name__ == "__main__":
     while time<sim_time:
         """lambda X: time*simple_shear()/sim_time"""
         #do timestep
-        dt,E_bal = dm.dd_step(suggested_dt,E_TOL=energy_tol, sig_ff=const_shear)
+        dt,E_bal = dm.dd_step(suggested_dt,E_TOL=energy_tol, sig_ff=lambda X: variable_shear(X,time/sim_time))
         time+=dt
         
         #adjust timestep based on last timestep
@@ -76,7 +73,7 @@ if __name__ == "__main__":
         print "\tenergy balance: ",E_bal
         #dm.dump()
         if time> next_plot_time:
-            dm.plot_w_stress("sig{0:04d}".format(plot_num),sig_ff=None) 
+            dm.plot_w_stress("sig{0:04d}".format(plot_num),sig_ff=lambda X: variable_shear(X,time/sim_time)) 
             #dm.plot("D{0:04d}".format(plot_num))  
             next_plot_time+=plot_res
             plot_num+=1